8640153ef8dd120272729266e04a6b1c892e6811
Bruno Costa <br9uno@gmail.com>
2026-06-28 20:43:18 -0300

    Configuracao routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
+const postsRoutes = require("./routes/postsRoutes");
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        mensagem: "API do Blog Tech Challenge funcionando!"
-    });
-});
+
+app.use("/", postsRoutes);
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
